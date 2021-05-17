f6c84bfa8b347afffff1b7208d5c54092acb5134
Akn <sibata486@gmail.com>
2021-05-18 01:45:39 +0900

    init

---
+++ Assets/Scenes/scripts/LoadText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LoadText : MonoBehaviour
+{
+    private TextAsset t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = Resources.Load("sample01/texts/testtext", typeof(TextAsset)) as TextAsset;
+        Debug.Log(t.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scenes/scripts/ShowText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowText : MonoBehaviour
+{
+
+    private Text textField;
+    private int frameCounter = 0;
+    private int characterCount = 0;
+
+    // Start is called before the first frame update
+    void Start() {
+        textField = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey("space")) {
+            textField.text = "";
+            characterCount = 0;
+        }
+
+        if(textField.text.Length > 200) {
+            return;
+        }
+
+        if(frameCounter % 3 == 0) {
+            textField.text += "a";
+        }
+
+        if(frameCounter > int.MaxValue - 1) {
+            frameCounter = 0;
+        }
+
+        frameCounter++;
+        characterCount++;
+    }
+}
