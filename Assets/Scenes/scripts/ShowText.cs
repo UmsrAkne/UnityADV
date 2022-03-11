6881890533c036aaaff6a7b8222c45a6573a5423
Akn <sibata486@gmail.com>
2022-03-11 22:42:01 +0900

    add / テキストを表示行うクラスを実装

--- Assets/Scenes/scripts/ShowText.cs
+++ Assets/Scenes/scripts/ShowText.cs
@@ -1,46 +1,74 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ShowText : MonoBehaviour
 {
-
     private Text textField;
-    private int frameCounter = 0;
-    private int characterCount = 0;
+    private int frameCounter;
+    private int characterCount;
+    private int position;
+    private bool writing;
+    private string currentText = string.Empty;
+
+    public List<string> Texts { get; set; } = new List<string>() { };
 
     // Start is called before the first frame update
     void Start()
     {
         textField = GetComponent<Text>();
+        textField.text = string.Empty;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            textField.text = "";
-            characterCount = 0;
+            ToNext();
+            return;
         }
 
-        if (textField.text.Length > 200)
+        if (frameCounter % 10 == 0 && writing)
+        {
+            textField.text += currentText[characterCount];
+            characterCount++;
+            frameCounter = 0;
+
+            if (characterCount >= currentText.Length)
+            {
+                writingComplete();
+            }
+        }
+
+        frameCounter++;
+    }
+
+    private void ToNext()
+    {
+        if (position >= Texts.Count)
         {
             return;
         }
 
-        if (frameCounter % 3 == 0)
-        {
-            textField.text += "a";
-        }
+        currentText = Texts[position];
 
-        if (frameCounter > int.MaxValue - 1)
+        if (writing)
         {
-            frameCounter = 0;
+            textField.text = currentText;
+            writingComplete();
         }
+        else
+        {
+            textField.text = string.Empty;
+            characterCount = 0;
+            writing = true;
+        }
+    }
 
-        frameCounter++;
-        characterCount++;
+    private void writingComplete()
+    {
+        position++;
+        writing = false;
     }
 }
