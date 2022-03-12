1bb3c00e824b9bc616483eda277bc9f7a7896b06
Akn <sibata486@gmail.com>
2022-03-12 22:48:28 +0900

    add / テキストファイルをロードするためのクラスを追加

---
+++ Assets/Scenes/scripts/loaders/TextLoader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class TextLoader
+{
+    public void Load(string targetPath)
+    {
+        try
+        {
+            XDocument xml = XDocument.Parse(File.ReadAllText(targetPath));
+
+            foreach (var data in xml.Root.Descendants())
+            {
+                Debug.Log(data);
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
