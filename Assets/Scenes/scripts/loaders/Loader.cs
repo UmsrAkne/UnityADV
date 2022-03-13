999722feacb865f3ecae49decb4699f80d4f2fc4
Akn <sibata486@gmail.com>
2022-03-13 23:18:20 +0900

    add / 読み込んだテキストデータを格納するクラスを追加

--- Assets/Scenes/scripts/loaders/Loader.cs
+++ Assets/Scenes/scripts/loaders/Loader.cs
@@ -6,12 +6,14 @@ using UnityEngine;
 public class Loader : MonoBehaviour
 {
 
+    Resource resource = new Resource();
     TextLoader textLoader = new TextLoader();
 
     // Start is called before the first frame update
     void Start()
     {
         textLoader.Load($@"{Directory.GetCurrentDirectory()}\scenes\sampleScn001\texts\scenario.xml");
+        resource.Scenarios = textLoader.Scenario;
     }
 
     // Update is called once per frame
