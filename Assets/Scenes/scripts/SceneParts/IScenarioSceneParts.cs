8f8f6dd88b577663faf72a857d53062978d36332
Akn <sibata486@gmail.com>
2022-03-14 23:31:36 +0900

    add /

---
+++ Assets/Scenes/scripts/SceneParts/IScenarioSceneParts.cs
@@ -0,0 +1,8 @@
+﻿public interface IScenarioSceneParts
+{
+    void execute();
+
+    void setScenario(Scenario scenario);
+
+    void setResource(Resource resource);
+}
---
+++ Assets/Scenes/scripts/SceneParts/TextWriter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : IScenarioSceneParts
+{
+    public Text Text { get; private set; }
+
+    private Scenario Scenario { get; set; }
+
+    private List<Scenario> Scenarios { get; set; }
+
+    public void execute()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void setScenario(Scenario scenario)
+    {
+        Scenario = scenario;
+    }
+
+    public void setResource(Resource resource)
+    {
+        Scenarios = resource.Scenarios;
+    }
+
+    public void setText(Text text)
+    {
+        if (Text == null)
+        {
+            Text = text;
+        }
+    }
+}
