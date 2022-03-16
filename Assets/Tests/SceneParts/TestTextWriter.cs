485465761e1f10b6ef8e25aec6554cb9887b25d3
Akn <sibata486@gmail.com>
2022-03-16 23:53:02 +0900

    add /

---
+++ Assets/Tests/SceneParts/TestTextWriter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestTextWriter
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestTextWriterSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        public void TestExecute()
+        {
+            // var res = new Resource();
+            // res.Scenarios = new List<Scenario>()
+            // {
+            //     new Scenario(){Text = "abcdef" },
+            //     new Scenario(){Text = "ghijkl" },
+            //     new Scenario(){Text = "mnopqr" },
+            //     new Scenario(){Text = "stuvwx" }
+            // };
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator TestTextWriterWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
