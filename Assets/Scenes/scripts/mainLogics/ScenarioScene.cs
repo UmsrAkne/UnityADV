0757a7c8c76cde677d6e05885cd521dbe529f157
Akn <sibata486@gmail.com>
2022-03-20 23:43:49 +0900

    add / メインロジックに TextWriter インスタンスを搭載

--- Assets/Scenes/scripts/mainLogics/ScenarioScene.cs
+++ Assets/Scenes/scripts/mainLogics/ScenarioScene.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ScenarioScene : MonoBehaviour
 {
+    public Resource Resource { private get; set; }
+
+    private List<IScenarioSceneParts> ScenarioSceneParts { get; } = new List<IScenarioSceneParts>();
+
+    private TextWriter TextWriter { get; } = new TextWriter();
+
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        ScenarioSceneParts.Add(TextWriter);
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            ScenarioSceneParts.ForEach(p =>
+            {
+                p.Execute();
+            });
+        }
+
+        ScenarioSceneParts.ForEach(p => p.ExecuteEveryFrame());
     }
 }
