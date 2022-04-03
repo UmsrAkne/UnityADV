562a994bdbb3b7e98e50a9fb0f0e68c91ea2784e
Akn <sibata486@gmail.com>
2022-04-04 02:10:30 +0900

    add / 透明度を変更するアニメーションクラスを追加

---
+++ Assets/Scenes/scripts/animations/AlphaChanger.cs
@@ -0,0 +1,42 @@
+﻿public class AlphaChanger : IAnimation
+{
+    private ImageSet target;
+    private int targetLayerIndex;
+
+    public bool IsWorking { get; private set; } = true;
+
+    public ImageSet Target
+    {
+        set
+        {
+            if (target == null)
+            {
+                target = value;
+            }
+        }
+
+        get => target;
+    }
+
+    public int TargetLayerIndex
+    {
+        get => targetLayerIndex;
+        set => targetLayerIndex = value;
+    }
+
+    public void Execute()
+    {
+        Target.Alpha += 0.05f;
+
+        if (Target.Alpha > 1.0)
+        {
+            IsWorking = false;
+        }
+    }
+
+    public void Stop()
+    {
+        IsWorking = false;
+        Target.Alpha = 1.0f;
+    }
+}
