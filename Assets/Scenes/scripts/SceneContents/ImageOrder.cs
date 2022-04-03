717fae83c661ca5ef6bd5cf8e7ad4fb0bd4577cf
Akn <sibata486@gmail.com>
2022-04-04 04:47:02 +0900

    add / 画像の拡大率を変更して表示できるよう実装

--- Assets/Scenes/scripts/SceneContents/ImageOrder.cs
+++ Assets/Scenes/scripts/SceneContents/ImageOrder.cs
@@ -7,4 +7,6 @@ public class ImageOrder
     public List<string> Names { get; private set; } = new List<string>();
 
     public int TargetLayerIndex { get; set; }
+
+    public double Scale { get; set; } = 1.0;
 }
