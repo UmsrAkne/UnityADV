80dbf9095230eb2065e5f9ddd4e4e3a83f6d2404
Akn <sibata486@gmail.com>
2022-05-03 22:26:27 +0900

    add / BGM を再生するクラスを追加

---
+++ Assets/Scenes/scripts/SceneParts/BGMPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class BGMPlayer : IScenarioSceneParts
+{
+    public bool NeedExecuteEveryFrame => false;
+
+    public void Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ExecuteEveryFrame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SetResource(Resource resource)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SetScenario(Scenario scenario)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SetUI(UI ui)
+    {
+        throw new System.NotImplementedException();
+    }
+}
