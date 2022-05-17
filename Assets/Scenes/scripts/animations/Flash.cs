7ea49b543d40971906b73809787f71ac667f5d72
Akn <sibata486@gmail.com>
2022-05-17 20:03:31 +0900

    add / Flash クラスを追加

---
+++ Assets/Scenes/scripts/animations/Flash.cs
@@ -0,0 +1,28 @@
+﻿namespace Animations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Flash : IAnimation
+    {
+        public string AnimationName => "flash";
+
+        public bool IsWorking => true;
+
+        public ImageSet Target { set; private get; }
+
+        public int TargetLayerIndex { get; set; }
+
+        public void Execute()
+        {
+        }
+
+        public void Stop()
+        {
+            UnityEngine.Debug.Log($"Flash : Execute Stop()");
+        }
+    }
+}
