5fdeaf33c1db314c485f202db16b3f1e4b38f172
Akn <sibata486@gmail.com>
2022-05-19 01:08:57 +0900

    add / インターフェース IDisplayObject を追加

---
+++ Assets/Scenes/scripts/SceneContents/IDisplayObject.cs
@@ -0,0 +1,15 @@
+﻿namespace SceneContents
+{
+    public interface IDisplayObject
+    {
+        float Alpha { get; set; }
+
+        double Scale { get; set; }
+
+        float X { get; set; }
+
+        float Y { get; set; }
+
+        int Angle { get; set; }
+    }
+}
