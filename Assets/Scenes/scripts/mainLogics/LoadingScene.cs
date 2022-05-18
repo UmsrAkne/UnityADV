45d4d350166dbb08346e4b3fb05ff0779a27b16c
Akn <sibata486@gmail.com>
2022-05-18 14:46:39 +0900

    add / 画像のロード処理の機能を強化
    一度ロードした画像のロードは行わないよう実装を変更
    また、無駄なゲームオブジェクト、コンポーネントの追加も行わないようにした

--- Assets/Scenes/scripts/mainLogics/LoadingScene.cs
+++ Assets/Scenes/scripts/mainLogics/LoadingScene.cs
@@ -2,8 +2,10 @@
 {
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using Loaders;
     using UnityEngine;
+    using UnityEngine.Rendering;
     using UnityEngine.SceneManagement;
     using UnityEngine.UI;
 
@@ -17,12 +19,18 @@
 
         private string[] Paths { get; set; }
 
+        private List<GameObject> GameObjects { get; } = new List<GameObject>();
+
+        private List<Sprite> Sprites { get; } = new List<Sprite>();
+
         // Start is called before the first frame update
         public void Start()
         {
             Text = GameObject.Find("TextWindow").GetComponent<Text>();
             Paths = Directory.GetDirectories($@"{Directory.GetCurrentDirectory()}\scenes");
             Text.text = Paths[cursorIndex];
+            Enumerable.Range(0, Paths.Length).ToList().ForEach(i => GameObjects.Add(new GameObject()));
+            Enumerable.Range(0, Paths.Length).ToList().ForEach(i => Sprites.Add(null));
             keyboardLock = false;
             LoadCurrentCursorImage();
         }
@@ -65,12 +73,29 @@
 
         private void LoadCurrentCursorImage()
         {
-            var sp = imageLoader.LoadImage($@"{Paths[cursorIndex]}\images\A0101.png", 1280, 720);
-            var emptyGameObject = new GameObject();
-            var imageSet = emptyGameObject.AddComponent<ImageSet>();
+            var imageSet = GameObjects[cursorIndex].GetComponent<ImageSet>();
 
-            imageSet.Sprites.Add(sp);
-            imageSet.Draw();
+            if (imageSet == null)
+            {
+                imageSet = GameObjects[cursorIndex].AddComponent<ImageSet>();
+            }
+
+            if (Sprites[cursorIndex] == null)
+            {
+                Sprites[cursorIndex] = imageLoader.LoadImage($@"{Paths[cursorIndex]}\images\A0101.png", 1280, 720);
+                imageSet.Sprites.Add(Sprites[cursorIndex]);
+                imageSet.Draw();
+            }
+
+            GameObjects.ForEach(g =>
+            {
+                if (g.GetComponent<ImageSet>() != null && g.GetComponent<SortingGroup>() != null)
+                {
+                    g.GetComponent<ImageSet>().GetComponent<SortingGroup>().sortingOrder = 0;
+                }
+            });
+
+            imageSet.GetComponent<SortingGroup>().sortingOrder = 1;
         }
     }
 }
