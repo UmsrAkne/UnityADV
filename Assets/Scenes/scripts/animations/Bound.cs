cf83173b19200d523dc5c8769bcf3fd9c228211c
Akn <sibata486@gmail.com>
2022-05-23 00:08:28 +0900

    add / Bound アニメーションを行うクラスを追加

---
+++ Assets/Scenes/scripts/animations/Bound.cs
@@ -0,0 +1,39 @@
+﻿namespace Animations
+{
+    using System;
+    using SceneContents;
+
+    public class Bound : IAnimation
+    {
+        private int frameCounter;
+
+        public string AnimationName => "bound";
+
+        public bool IsWorking => true;
+
+        public IDisplayObject Target { get; set; }
+
+        public ImageContainer TargetContainer { get; set; }
+
+        public int TargetLayerIndex { get; set; }
+
+        public int Duration { get; set; }
+
+        public int Degree { get; set; }
+
+        public int Strength { get; set; }
+
+        public void Execute()
+        {
+            if (Target == null || !IsWorking)
+            {
+                return;
+            }
+
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
