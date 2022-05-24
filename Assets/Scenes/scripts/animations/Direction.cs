b2caa7a1d7eee33a561533e64b0ba87846db3bfc
Akn <sibata486@gmail.com>
2022-05-24 19:14:23 +0900

    add / 方向を表す列挙型を追加

---
+++ Assets/Scenes/scripts/animations/Direction.cs
@@ -0,0 +1,31 @@
+﻿namespace Animations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public enum Direction
+    {
+        up = 0,
+
+        upRight = 1,
+        rightUp = 1,
+
+        right = 2,
+
+        downRight = 3,
+        rightDown = 3,
+
+        down = 4,
+
+        downLeft = 5,
+        leftDown = 5,
+
+        left = 6,
+
+        upLeft = 7,
+        leftUp = 7,
+    }
+}
