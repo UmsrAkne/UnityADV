58a842f502c0cdccbec0df0f2f538718495feb6b
Akn <sibata486@gmail.com>
2022-05-25 23:51:16 +0900

    add / bgvタグを変換するクラスを追加

---
+++ Assets/Scenes/scripts/loaders/BackgroundVoiceElementConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace Loaders
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+    using SceneContents;
+
+    public class BackgroundVoiceElementConverter : IXMLElementConverter
+    {
+        public string TargetElementName => "backgroundVoice";
+
+        public List<string> Log => new List<string>();
+
+        public void Convert(XElement xmlElement, Scenario scenario)
+        {
+            var tags = xmlElement.Elements(TargetElementName);
+
+            if (tags.Count() != 0)
+            {
+                foreach (XElement bgvTag in tags)
+                {
+                }
+            }
+        }
+    }
+}
