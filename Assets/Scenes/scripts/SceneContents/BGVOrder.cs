2609a6d538b6411753c575ae9a6f6aaebe6e2493
Akn <sibata486@gmail.com>
2022-05-25 23:54:16 +0900

    add / BGV の命令を表すクラスを追加

---
+++ Assets/Scenes/scripts/SceneContents/BGVOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace SceneContents
+{
+    using System.Collections.Generic;
+
+    public class BGVOrder
+    {
+        public List<string> FileNames { get; set; } = new List<string>();
+
+        public bool IsStopOrder { get; set; }
+    }
+}
