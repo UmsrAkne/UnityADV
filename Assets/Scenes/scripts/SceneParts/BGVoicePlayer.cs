266be986970d04747544fb68f81d5fea2ffa2a34
Akn <sibata486@gmail.com>
2022-05-26 01:12:04 +0900

    add / bgv を再生するシーンパーツを追加

---
+++ Assets/Scenes/scripts/SceneParts/BGVoicePlayer.cs
@@ -0,0 +1,58 @@
+﻿namespace SceneParts
+{
+    using System;
+    using System.Collections.Generic;
+    using SceneContents;
+
+    public class BGVoicePlayer : IScenarioSceneParts
+    {
+        private BGVOrder currentOrder;
+        private bool playRequest;
+        private VoicePlayer voicePlayer;
+        private int channel;
+        private List<ISound> bgVoices;
+        private Dictionary<string, ISound> bgVoicesByName;
+
+        public BGVoicePlayer(VoicePlayer voicePlayer)
+        {
+            this.voicePlayer = voicePlayer;
+            channel = voicePlayer.Channel;
+        }
+
+        public bool NeedExecuteEveryFrame => true;
+
+        public void Execute()
+        {
+            if (!playRequest)
+            {
+                return;
+            }
+        }
+
+        public void ExecuteEveryFrame()
+        {
+        }
+
+        public void SetResource(Resource resource)
+        {
+            bgVoices = resource.BGVoices;
+            bgVoicesByName = resource.BGVoicesByName;
+        }
+
+        public void SetScenario(Scenario scenario)
+        {
+            scenario.BGVOrders.ForEach(order =>
+            {
+                if (order.Channel == channel)
+                {
+                    currentOrder = order;
+                    playRequest = true;
+                }
+            });
+        }
+
+        public void SetUI(UI ui)
+        {
+        }
+    }
+}
