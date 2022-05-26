0e6a90a94324f2eca274001c37b4775cc0a9d74e
Akn <sibata486@gmail.com>
2022-05-26 20:13:02 +0900

    add / BGVPlayer の機能を実装

--- Assets/Scenes/scripts/SceneParts/BGVoicePlayer.cs
+++ Assets/Scenes/scripts/SceneParts/BGVoicePlayer.cs
@@ -2,24 +2,49 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
     using SceneContents;
 
     public class BGVoicePlayer : IScenarioSceneParts
     {
+        // 一度音声の再生を開始したら、100ms は ExecuteEveryFrame() の処理をしない。
+        // この待機時間を設けないと、AudioSource.IsPlaying の仕様により、音声が多重再生される。
+        private readonly int playWaitTime = 100;
+
         private BGVOrder currentOrder;
+        private ISound playingVoice;
+        private List<ISound> playingVoiceList;
         private bool playRequest;
-        private VoicePlayer voicePlayer;
         private int channel;
-        private List<ISound> bgVoices;
         private Dictionary<string, ISound> bgVoicesByName;
+        private double volume;
+        private int playingVoiceIndex;
+        private Stopwatch playElapsedTimer = new Stopwatch();
+        private bool increasingVolume;
+        private bool playing;
 
         public BGVoicePlayer(VoicePlayer voicePlayer)
         {
-            this.voicePlayer = voicePlayer;
             channel = voicePlayer.Channel;
+            voicePlayer.SoundComplete += VoiceCompleteEventHandler;
         }
 
         public bool NeedExecuteEveryFrame => true;
+
+        public double Volume
+        {
+            get => volume;
+            private set
+            {
+                if (playingVoice != null)
+                {
+                    playingVoice.Volume = value;
+                }
+
+                volume = value;
+            }
+        }
 
         public void Execute()
         {
@@ -27,15 +52,48 @@
             {
                 return;
             }
+
+            playingVoiceList = currentOrder.FileNames.Select(name => bgVoicesByName[name]).OrderBy(_ => Guid.NewGuid()).ToList();
+            playingVoice = playingVoiceList.First();
+            playElapsedTimer.Restart();
         }
 
         public void ExecuteEveryFrame()
         {
+            if (increasingVolume && volume < 1.0)
+            {
+                Volume += 0.02;
+            }
+
+            if (playingVoice == null || playingVoice.IsPlaying || playElapsedTimer.ElapsedMilliseconds <= playWaitTime)
+            {
+                return;
+            }
+
+            playingVoiceIndex++;
+
+            if (playingVoiceList.Count <= playingVoiceIndex)
+            {
+                playingVoiceIndex = 0;
+
+                if (playingVoiceList.Count >= 3)
+                {
+                    var exceptLast = playingVoiceList.GetRange(0, playingVoiceList.Count - 1);
+                    exceptLast = exceptLast.OrderBy(_ => Guid.NewGuid()).ToList();
+                    exceptLast.Insert(new Random().Next(1, exceptLast.Count - 1), playingVoiceList.Last());
+
+                    playingVoiceList = exceptLast;
+                }
+            }
+
+            playingVoice = playingVoiceList[playingVoiceIndex];
+            playingVoice.Volume = Volume;
+            playingVoice.Play();
+            playElapsedTimer.Restart();
         }
 
         public void SetResource(Resource resource)
         {
-            bgVoices = resource.BGVoices;
             bgVoicesByName = resource.BGVoicesByName;
         }
 
@@ -47,12 +105,30 @@
                 {
                     currentOrder = order;
                     playRequest = true;
+                    playing = true;
                 }
             });
+
+            if (scenario.VoiceOrders.Any(o => o.Channel == channel))
+            {
+                // 同一チャンネルの音声再生の指示がある場合は、このプレイヤーの音量は 0 に変更
+                Volume = 0;
+                increasingVolume = false;
+            }
         }
 
         public void SetUI(UI ui)
         {
+        }
+
+        private void VoiceCompleteEventHandler(object sender, EventArgs e)
+        {
+            if (playRequest || playing)
+            {
+                increasingVolume = true;
+            }
+
+            playRequest = false;
         }
     }
 }
