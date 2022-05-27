c872375fd4e7d892df212cf4d19f2d0a378e2ac1
Akn <sibata486@gmail.com>
2022-05-27 18:34:27 +0900

    add / Stop タグを読み込むためのクラスを追加

---
+++ Assets/Scenes/scripts/loaders/StopElementConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace Loaders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+    using SceneContents;
+
+    public class StopElementConverter : IXMLElementConverter
+    {
+        public string TargetElementName => "stop";
+
+        public List<string> Log => new List<string>();
+
+        public void Convert(XElement xmlElement, Scenario scenario)
+        {
+            var tags = xmlElement.Elements(TargetElementName);
+
+            if (tags.Count() != 0)
+            {
+                foreach (XElement stopTag in tags)
+                {
+                }
+            }
+        }
+    }
+}
