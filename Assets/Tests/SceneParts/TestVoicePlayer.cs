53502472def649866d876a295d349f722b6f3969
Akn <sibata486@gmail.com>
2022-05-29 14:11:13 +0900

    add / VoicePlayer のテストクラスと、必要なダミークラスを追加

---
+++ Assets/Tests/SceneParts/TestVoicePlayer.cs
@@ -0,0 +1,18 @@
+﻿namespace Tests
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using Assets.Tests.SceneContents;
+    using NUnit.Framework;
+    using SceneContents;
+    using SceneParts;
+    using UnityEngine.TestTools;
+
+    public class TestVoicePlayer
+    {
+        [Test]
+        public void ボイス再生のテスト()
+        {
+        }
+    }
+}
