c9d562e811af355351afff4574d96ed410fc7494
Akn <sibata486@gmail.com>
2022-06-05 00:10:27 +0900

    add / ImageSet.Draw() にList<Sprite>のオーバーロードを追加

--- Assets/Scenes/scripts/SceneContents/ImageSet.cs
+++ Assets/Scenes/scripts/SceneContents/ImageSet.cs
@@ -72,6 +72,8 @@
 
         private List<SpriteRenderer> Renderers { get; set; } = new List<SpriteRenderer>();
 
+        private List<GameObject> GameObjects { get; set; } = new List<GameObject>(4) { null, null, null, null };
+
         public void Draw()
         {
             var container = this.gameObject;
@@ -105,6 +107,44 @@
 
             Renderers[0].sortingOrder = -1;
             Renderers[0].maskInteraction = SpriteMaskInteraction.VisibleOutsideMask;
+
+            Alpha = alpha;
+            Scale = scale;
+        }
+
+        public void Draw(List<Sprite> sprites)
+        {
+            var container = this.gameObject;
+            var sg = gameObject.AddComponent<SortingGroup>();
+            sg.sortingLayerName = $"Layer_{SortingLayerIndex}";
+
+            for (var i = 0; i < sprites.Count; i++)
+            {
+                var sp = sprites[i];
+
+                if (sp == null)
+                {
+                    continue;
+                }
+
+                var g = new GameObject();
+                GameObjects[i] = g;
+
+                g.transform.SetParent(container.transform, false);
+                var r = g.AddComponent<SpriteRenderer>();
+                Renderers.Add(r);
+                r.sprite = sp;
+
+                if (i == 0)
+                {
+                    r.sortingOrder = -1;
+                    r.maskInteraction = SpriteMaskInteraction.VisibleOutsideMask;
+                }
+                else
+                {
+                    g.AddComponent<SpriteMask>().sprite = r.sprite;
+                }
+            }
 
             Alpha = alpha;
             Scale = scale;
@@ -157,6 +197,7 @@
             MaskObject?.SetActive(false);
 
             gos.ForEach(g => g.SetActive(false));
+            GameObjects.ForEach(g => g.SetActive(false));
         }
     }
 }
