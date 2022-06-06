4850332c8e3360e8774ea8066e68bad75f4f9165
Akn <sibata486@gmail.com>
2022-06-06 23:01:27 +0900

    add / ImageContainer 内で Sprite を扱いやすくするためのクラスを追加

---
+++ Assets/Scenes/scripts/SceneContents/ImageUnit.cs
@@ -0,0 +1,14 @@
+﻿namespace SceneContents
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using UnityEngine;
+
+    public class ImageUnit
+    {
+        public GameObject GameObject { get; private set; }
+    }
+}
