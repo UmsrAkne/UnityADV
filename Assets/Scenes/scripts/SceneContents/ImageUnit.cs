8e30413fedde2cd7931fc8cf66c696e919072db5
Akn <sibata486@gmail.com>
2022-06-06 23:47:56 +0900

    add / ImageUnit の処理を実装

--- Assets/Scenes/scripts/SceneContents/ImageUnit.cs
+++ Assets/Scenes/scripts/SceneContents/ImageUnit.cs
@@ -1,14 +1,55 @@
 ﻿namespace SceneContents
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using UnityEngine;
+    using UnityEngine.Rendering;
 
     public class ImageUnit
     {
-        public GameObject GameObject { get; private set; }
+        private GameObject gameObject;
+        private SpriteRenderer spriteRenderer;
+        private SortingGroup sortingGroup;
+
+        public GameObject GameObject
+        {
+            get
+            {
+                gameObject = gameObject ?? new GameObject();
+                return gameObject;
+            }
+        }
+
+        public SpriteRenderer SpriteRenderer
+        {
+            get
+            {
+                spriteRenderer = spriteRenderer ?? GameObject.AddComponent<SpriteRenderer>();
+                return spriteRenderer;
+            }
+        }
+
+        public SortingGroup SortingGroup
+        {
+            get
+            {
+                sortingGroup = sortingGroup ?? GameObject.AddComponent<SortingGroup>();
+                return sortingGroup;
+            }
+        }
+
+        public void SetMaskSprite(Sprite sprite)
+        {
+            var spriteMask = GameObject.GetComponent<SpriteMask>();
+            if (spriteMask == null)
+            {
+                spriteMask = GameObject.AddComponent<SpriteMask>();
+            }
+
+            spriteMask.sprite = sprite;
+        }
+
+        public void SetParent(GameObject parent)
+        {
+            GameObject.transform.SetParent(parent.transform, false);
+        }
     }
 }
