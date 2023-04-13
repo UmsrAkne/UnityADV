bda9adbef6bf082ea707f6b081e7d1751958334d
Akn <sibata486@gmail.com>
2023-04-14 00:41:21 +0900

    remove Interface / SlideCore に実装されていた IAnimation を解除
    必然性のない実装。余分なプロパティを削除するため

--- Assets/Scenes/scripts/animations/SlideCore.cs
+++ Assets/Scenes/scripts/animations/SlideCore.cs
@@ -4,7 +4,7 @@
     using System.Numerics;
     using SceneContents;
 
-    public class SlideCore : IAnimation
+    public class SlideCore
     {
         private int executeCounter;
         private double totalDistance;
