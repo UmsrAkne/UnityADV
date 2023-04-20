a1c91cab9b9424efffb45823256d6da7ebacb012
Akn <sibata486@gmail.com>
2023-04-21 01:12:15 +0900

    add feat / AnimationChain の機能を実装

--- Assets/Tests/Animations/TestAnimationChain.cs
+++ Assets/Tests/Animations/TestAnimationChain.cs
@@ -1,3 +1,4 @@
+using System.Xml.Linq;
 using NUnit.Framework;
 using Scenes.Scripts.Animations;
 
@@ -21,17 +22,9 @@ namespace Tests.Animations
             {
                 Target = dummyDisplayObject
             };
-;
-            chain.AddAnimation(new AlphaChanger()
-            {
-                Amount = 0.1,
-            });
 
-            chain.AddAnimation(new Slide()
-            {
-                Speed = 5,
-                Distance = 200,
-            });
+            chain.AddAnimationTag(XElement.Parse("<anime name=\"alphaChanger\" amount=\"0.1\" />"));
+            chain.AddAnimationTag(XElement.Parse("<anime name=\"slide\" speed=\"5\" distance=\"200\" />"));
 
             for (int i = 0; i < 10; i++)
             {
@@ -49,6 +42,58 @@ namespace Tests.Animations
 
             Assert.Greater(dummyDisplayObject.Alpha, 1.0, "AlphaChanger は停止しているので、一度目のチェックと同じ値");
             Assert.Greater(dummyDisplayObject.Y, 200);
+            Assert.IsFalse(chain.IsWorking);
+        }
+
+        [Test]
+        public void ループ動作テスト()
+        {
+            var dummyDisplayObject = new DummyDisplayObject
+            {
+                Y = 0
+            };
+
+            var chain = new AnimationChain
+            {
+                Target = dummyDisplayObject,
+                RepeatCount = 1,
+            };
+
+            chain.AddAnimationTag(XElement.Parse("<anime name=\"slide\" speed=\"5\" distance=\"100\" />"));
+            chain.AddAnimationTag(XElement.Parse("<anime name=\"slide\" speed=\"5\" distance=\"200\" />"));
+
+            for (int i = 0; i < 300; i++)
+            {
+                chain.Execute();
+            }
+
+            Assert.Greater(dummyDisplayObject.Y, 600);
+            Assert.IsFalse(chain.IsWorking);
+        }
+
+        [Test]
+        public void ループ動作テスト２回()
+        {
+            var dummyDisplayObject = new DummyDisplayObject
+            {
+                Y = 0
+            };
+
+            var chain = new AnimationChain
+            {
+                Target = dummyDisplayObject,
+                RepeatCount = 2,
+            };
+
+            chain.AddAnimationTag(XElement.Parse("<anime name=\"slide\" speed=\"5\" distance=\"100\" />"));
+            chain.AddAnimationTag(XElement.Parse("<anime name=\"slide\" speed=\"5\" distance=\"200\" />"));
+
+            for (int i = 0; i < 300; i++)
+            {
+                chain.Execute();
+            }
+
+            Assert.Greater(dummyDisplayObject.Y, 900);
             Assert.IsFalse(chain.IsWorking);
         }
     }
