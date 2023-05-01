99354ef151b483726b60c8c76d27b410f257162a
Akn <sibata486@gmail.com>
2023-05-02 00:24:11 +0900

    add files / 新しい SlideCore クラスを追加

---
+++ Assets/Scenes/Scripts/animations/SlideCoreB.cs
@@ -0,0 +1,23 @@
+using Scenes.Scripts.SceneContents;
+
+namespace Scenes.Scripts.Animations
+{
+    public class SlideCoreB
+    {
+        public bool IsWorking { get; private set; } = true;
+
+        public IDisplayObject Target { get; set; }
+
+        public void Execute()
+        {
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
