205c5702f43d91746f214a71bf864aa758709406
Akn <sibata486@gmail.com>
2023-05-02 20:44:35 +0900

    add feat / SlideCoreB の機能を実装

--- Assets/Scenes/Scripts/animations/SlideCoreB.cs
+++ Assets/Scenes/Scripts/animations/SlideCoreB.cs
@@ -1,15 +1,68 @@
+using System;
 using Scenes.Scripts.SceneContents;
 
 namespace Scenes.Scripts.Animations
 {
     public class SlideCoreB
     {
+        private double beforeDistance;
+        private double endPos = 0;
+        private int sideCutting = 5;
+        private double startPos = 0;
+        private double totalLength = 0;
+        private double x;
+        private double y;
+
         public bool IsWorking { get; private set; } = true;
 
         public IDisplayObject Target { get; set; }
 
+        public int Duration { get; set; }
+
+        public int Distance { get; set; }
+
+        public int Degree { get; set; }
+
+        public int ExecuteCounter { get; private set; }
+
+        /// <summary>
+        /// 移動時に使うロジックの中で使用する Math.Sin グラフの左右部分をそれぞれ指定した範囲だけ切り取ります。
+        /// この値を大きくすると、速度変化の勾配が急になります。
+        /// 値は 0 - 10 の間での使用を想定。デフォルトは 5
+        /// </summary>
+        public int SideCutting { get => sideCutting; set => sideCutting = value; }
+
         public void Execute()
         {
+            if (!IsWorking)
+            {
+                return;
+            }
+
+            if (ExecuteCounter == 0)
+            {
+                startPos = GetPositionRatio(SideCutting);
+                endPos = GetPositionRatio(90.0 - SideCutting);
+                totalLength = endPos - startPos;
+            }
+
+            ExecuteCounter++;
+
+            var d = Distance * Core(ExecuteCounter);
+
+            var beforeX = x;
+            var beforeY = y;
+
+            x = Math.Cos(Degree * Math.PI / 180) * d;
+            y = Math.Sin(Degree * Math.PI / 180) * d;
+
+            Target.X += (float)(x - beforeX);
+            Target.Y += (float)(y - beforeY);
+
+            if (ExecuteCounter >= Duration)
+            {
+                Stop();
+            }
         }
 
         public void Start()
@@ -18,6 +71,39 @@ namespace Scenes.Scripts.Animations
 
         public void Stop()
         {
+            IsWorking = false;
+            Duration = 0;
+            Distance = 0;
+        }
+
+        public double GetPositionRatio(double counter)
+        {
+            var deg = (counter - 45.0) * 2.0;
+
+            // Math.Sin((counter - 45.0) * 2.0) の計算で、 counter をインクリメントすると -1 --> 1 が返る
+            var sin = Math.Sin(deg * (Math.PI / 180));
+
+            // sin の値は -1...1 となる。これを 0...1 に変換する計算を行っている。
+            return (sin + 1.0) / 2.0;
+        }
+
+        /// <summary>
+        /// 0 - 1.0 の値を返却します。
+        /// </summary>
+        /// <param name="counter">0 から Duration までの値を入力します。</param>
+        /// <returns>counter の値が 0　に近いほど 0 に近い値が出力され、Duration に近いほど 1.0 に近い値が出力されます</returns>
+        private double Core(int counter)
+        {
+            var cnt = (90.0 - SideCutting * 2) / Duration;
+
+            // 入力する値は sideCutting で下駄を履かせた counter に cnt をかける。
+            // ここでの入力値は常に sideCutting から (90度 - sideCutting) の値の間の値となる
+            var r = GetPositionRatio(SideCutting + counter * cnt);
+
+            // 先程の値からスタート地点の値を引く。すると counter == 0 のとき、 r == 0
+            // r の値は、常に (0 >= r <= totalLength)
+            // totalLength に対する r の割合を返却する。
+            return (r - startPos) / totalLength;
         }
     }
 }
--- Assets/Tests/Animations/TestSlideCoreB.cs
+++ Assets/Tests/Animations/TestSlideCoreB.cs
@@ -1,7 +1,29 @@
+using System;
+using NUnit.Framework;
+using Scenes.Scripts.Animations;
+
 namespace Tests.Animations
 {
     public class TestSlideCoreB
     {
+        [Test]
+        public void GetPositionRatioテスト()
+        {
+            var dummy = new DummyDisplayObject();
+            var slideCore = new SlideCoreB
+            {
+                Duration = 40,
+                Distance = 100,
+                Target = dummy
+            };
 
+            for (int i = 0; i < 50; i++)
+            {
+                slideCore.Execute();
+            }
+
+            Assert.Less(Math.Abs(dummy.X - 100.0), 0.5);
+            Assert.AreEqual(0, dummy.Y);
+        }
     }
 }
