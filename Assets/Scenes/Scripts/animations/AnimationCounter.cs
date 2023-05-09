9234bcd2911c033222146b1ce185c920c1e2d230
Akn <sibata486@gmail.com>
2023-05-09 22:33:22 +0900

    add class / 実行回数のカウントを行うクラスを追加

---
+++ Assets/Scenes/Scripts/animations/AnimationCounter.cs
@@ -0,0 +1,61 @@
+using Scenes.Scripts.SceneContents;
+
+namespace Scenes.Scripts.Animations
+{
+    public class AnimationCounter
+    {
+        public bool IsWorking { get; private set; } = true;
+
+        public int RepeatCount { get; set; }
+
+        public int Delay { get; set; }
+
+        public int Interval { get; set; }
+
+        public int Duration { get; set; }
+
+        public bool CanProcess { get; set; }
+
+        /// <summary>
+        /// CountUp() を実行した回数です。
+        /// この値は動作中にリセットされる場合があるため、必ずしも実行した合計回数が取得できるわけではありません
+        /// 合計回数が必要な場合は TotalExecutionCounter を使ってください。
+        /// </summary>
+        public int ExecutionCounter { get; private set; }
+
+        /// <summary>
+        /// CountUp() を実行した合計回数です。この値はいかなる場合もリセットされません。
+        /// </summary>
+        public int TotalExecutionCounter { get; private set; }
+
+        public void CountUp()
+        {
+            TotalExecutionCounter++;
+
+            if (Delay > 0)
+            {
+                Delay--;
+                CanProcess = false;
+                return;
+            }
+
+            ExecutionCounter++;
+            CanProcess = true;
+
+            if (Duration > 0 && ExecutionCounter >= Duration)
+            {
+                ExecutionCounter = 0;
+
+                if (RepeatCount > 0)
+                {
+                    RepeatCount--;
+                    Delay = Interval;
+                    return;
+                }
+
+                IsWorking = false;
+                CanProcess = false;
+            }
+        }
+    }
+}
